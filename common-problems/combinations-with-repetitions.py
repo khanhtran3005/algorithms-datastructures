fd2613d5ff2c25ee0d807e2897ef2f8854cf52e3
Khanh <khanh.tran3005@gmail.com>
2018-11-15 16:43:11 +0700

    combinations with repetitions

---
+++ common-problems/combinations-with-repetitions.py
@@ -0,0 +1,28 @@
+def combination(chars, k):
+	n = len(chars)
+	data = ['']*k
+	combinationHelper(chars, k, data, 0, n - 1, 0)
+
+
+def combinationHelper(chars, k, data, start, end, index):
+	if index == k:
+		print(indent(index), toString(data))
+	else:
+		i = start
+		# and end - i + 1 >= k - index (optional, remove redundant loops)
+		# while i <= end:
+		while i <= end:
+			data[index] = chars[i]
+			print('start: ', start, 'i: ', i, 'index: ', index, indent(index), data)
+
+			combinationHelper(chars, k, data, i, end, index + 1)
+			data[index] = ''
+			i += 1
+
+def toString(chars):
+    return ''.join(chars)
+
+def indent(n):
+    return '    '*n
+
+combination(['a', 'b', 'c', 'd'], 2)
