309abf84fb100667ff46aeeafc6f6b7e6e7863fe
Khanh Tran <khanh.tran3005@gmail.com>
2018-11-15 16:35:19 +0700

    file rename

--- common-problems/permutations-of-size-n.py
+++ common-problems/permutations-of-size-n.py
@@ -1,20 +1,20 @@
-def combinations(string, k):
+def permutations(string, k):
     chars = list(string)
     chosen = ['']*len(string)
-    return combinationsHelper(chars, k, chosen)
+    return permutationsHelper(chars, k, chosen)
 
 
-def combinationsHelper(chars, k, chosen, index=0):
+def permutationsHelper(chars, k, chosen, index=0):
     if index == k:
         print(toString(chosen))
 
     else:
         for i in range(len(chars)):
             chosen.append(chars.pop(i))
-            combinationsHelper(chars, k, chosen, index+1)
+            permutationsHelper(chars, k, chosen, index+1)
             chars.insert(i, chosen.pop())
 
 def toString(chars):
     return ''.join(chars)
 
-combinations('ABCD', 2)
+permutations('ABCD', 2)
