34282d4a3721be7236eb256116ecd257e980cd8b
Khanh <khanh.tran3005@gmail.com>
2018-11-15 16:40:20 +0700

    validate whether a tree is a BST or not

---
+++ trees/is_binary_search_tree.py
@@ -0,0 +1,14 @@
+def isValidBST(tree):
+
+	if tree.hasLeftChild():
+		if tree.getRootValue() < tree.getLeftChild().getRootValue():
+			return False
+
+		return isValidBST(tree.getLeftChild())
+	if tree.hasRightChild():
+		if tree.getRootValue() > tree.getRightChild.getRootValue()
+			return False
+
+		return isValidBST(tree.getRightChild())
+
+	return True
